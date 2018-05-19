926e2bd8b027ed95f1453d70b21db4f72a515cd3
Mehul Patel <mehul.2190@gmail.com>
2018-05-19 11:45:20 -0400

    Added more contents to housing.py
    Signed-off-by: Mehul Patel <mehul.2190@gmail.com>

--- Chapter 2/src/housing.py
+++ Chapter 2/src/housing.py
@@ -6,6 +6,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import Imputer
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -39,9 +42,6 @@ housing.info()
 housing["ocean_proximity"].value_counts()
 housing.describe()
 
-housing.hist(bins=50, figsize=(20,15))
-plt.show()
-
 
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
@@ -55,3 +55,44 @@ def split_train_test(data, test_ratio):
 #print(len(train_set), "training +", len(test_set), "test.")
 
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=21)
+housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+#housing.hist(bins=50, figsize=(20,15))
+#plt.show()
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=21)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+housing = strat_train_set.copy()
+#housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+#housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, s=housing["population"]/100, label="population", figsize=(10, 7),
+            #c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
+plt.legend()
+plt.show()
+
+corr_matrix = housing.corr()
+attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12,8))
+plt.show()
+
+# Attribute combinations
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"] = housing["population"]/housing["households"]
+
+corr_matrix = housing.corr()
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+housing = strat_train_set.drop("median_house_value", axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+imputer = Imputer(strategy="median")
+housing_num = housing.drop("ocean_proximity", axis=1)
+imputer.fit(housing_num)
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns)
